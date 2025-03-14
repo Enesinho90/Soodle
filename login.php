d2361425e469522ed6d258938b9e94c2bf25d754
Enes UZUN <124529459+Enesinho90@users.noreply.github.com>
2025-03-14 19:43:31 +0100

    Update login.php

--- login.php
+++ login.php
@@ -2,27 +2,26 @@
 <html lang="fr">
 <head>
     <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
-
+    <script src="https://kit.fontawesome.com/4162108d67.js" crossorigin="anonymous"></script>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Se connecter | Soodle</title>
 </head>
 <body>
 <div class="connexion">
-    <h1>Connexion à Soodle</h1>
+
     <form  onsubmit="return validateForm()">
         <input type="text" placeholder="Nom d'utilisateur" id="login" required > <br>
-        <input type="password" placeholder="Mot de passe" id="mdp" required ><br>
+        <input type="password" placeholder="Mot de passe" id="mdp" required >
+        <i id="eye" class=" fa-solid fa-eye-slash" onclick="switchEyes()" role="button" aria-label="Afficher/Masquer le mot de passe"></i>
+
         <input type="submit" value="Connexion">
     </form>
-    <p><a href=""> Mot de passe oublié ?</a></p>
+
 </div>
 
 
-    <script>
-
-    </script>
-
+<script src="script.js"></script>
 </body>
+
 </html>
