567841bfdad4619074dec649af92d4c250884452
Enes UZUN <124529459+Enesinho90@users.noreply.github.com>
2025-03-14 14:44:34 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Se connecter | Soodle</title>
+</head>
+<body>
+<div class="connexion">
+    <h1>Connexion à Soodle</h1>
+    <form  onsubmit="return validateForm()">
+        <input type="text" placeholder="Nom d'utilisateur" id="login" required > <br>
+        <input type="password" placeholder="Mot de passe" id="mdp" required ><br>
+        <input type="submit" value="Connexion">
+    </form>
+    <p><a href=""> Mot de passe oublié ?</a></p>
+</div>
+
+
+    <script>
+
+    </script>
+
+</body>
+</html>
