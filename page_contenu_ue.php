1308530c081e49e7f8c373d13f14c08a74f7c4f8
VarmazE <varmazea@gmail.com>
2025-03-23 22:36:14 +0100

    Modifiction sur le contenu UE

--- page_contenu_ue.php
+++ page_contenu_ue.php
@@ -3,17 +3,59 @@
 <head>
   <meta charset="UTF-8">
   <title>Contenu UE</title>
+  <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/navbar.css">
+  <script src="https://kit.fontawesome.com/4162108d67.js" crossorigin="anonymous"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+
 </head>
 <body id="page-contenu-ue">
-
+  <main>
+    <?php include("initialize.php"); ?>
+    <main>
   <?php include("initialize.php"); ?>
 
-  <h1>Bienvenue dans l'UE</h1>
+  <!-- ====== Section : contenu-ue ====== -->
+  <section id="contenu-ue">
+    
+    <!-- Bouton de retour -->
+    <button class="button-style" type="button">Retour</button>
+    
+    <!-- Titre principal -->
+    <h1>WE4A - Développement Web</h1>
 
-  <button id="test" type="button">Hallo</button>
+    <!-- Sous-titre participants -->
+    <p class="participants">Participants</p>
+
+    <!-- Titre des publications -->
+    <h2 class="publications">Publications</h2>
+
+    <!-- Zone de recherche + bouton d'ajout -->
+    <div class="barre-recherche-ajouter">
+      
+      <!-- Barre de recherche -->
+      <div class="barre-recherche-container">
+        <input class="barre-recherche" type="text" placeholder="Recherche un post">
+        <button class="recherche-btn">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </div>
+
+      <!-- Bouton d'ajout -->
+      <button class="button-add" type="button">
+        <i class="fa-solid fa-plus"></i>
+      </button>
+    </div>
+  </section>
+  <!-- ====== Fin section : contenu-ue ====== -->
+</main>
+
+
+  </main>
+
+  
+
 
   <script src="js/script.js"></script>
-  
 </body>
 </html>
