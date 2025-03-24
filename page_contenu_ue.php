e99b3d97d5f2832ee61ae40599219d0e057dd3da
VarmazE <varmazea@gmail.com>
2025-03-24 16:10:11 +0100

    Posts Ajouter

--- page_contenu_ue.php
+++ page_contenu_ue.php
@@ -10,28 +10,19 @@
 
 </head>
 <body id="page-contenu-ue">
-
-<main>
-  <?php include("initialize.php"); ?>
-
+<main> <?php include("initialize.php"); ?>
   <!-- ====== Section : contenu-ue ====== -->
   <section id="contenu-ue">
-    
     <!-- Bouton de retour -->
     <button class="button-style" type="button">Retour</button>
-    
     <!-- Titre principal -->
     <h1>WE4A - Développement Web</h1>
-
     <!-- Sous-titre participants -->
     <p class="participants">Participants</p>
-
     <!-- Titre des publications -->
     <h2 class="publications">Publications</h2>
-
     <!-- Zone de recherche + bouton d'ajout -->
     <div class="barre-recherche-ajouter">
-      
       <!-- Barre de recherche -->
       <div class="barre-recherche-container">
         <input class="barre-recherche" type="text" placeholder="Recherche un post">
@@ -39,7 +30,6 @@
           <i class="fa-solid fa-magnifying-glass"></i>
         </button>
       </div>
-
       <!-- Bouton d'ajout -->
       <button class="button-add" type="button">
         <i class="fa-solid fa-plus"></i>
@@ -47,6 +37,41 @@
     </div>
   </section>
   <!-- ====== Fin section : contenu-ue ====== -->
+  
+  
+  
+  <!-- ====== Section : Posts ====== -->
+  <section id="posts">
+    <!-- Un post -->
+    <div class="post">
+      <div class="titre-logo">
+        <i class="fa-regular fa-message"></i>
+        <h3>TD1 - Validation/Remise à Niveau</h3>
+      </div>
+      <p>Posté par M.Dupont il y a 2j</p>
+      <div class="contenu-message"> Le prochain cours se déroulera en B204 </div>
+      <div class="ajouter-modifier">
+        <button class="button-ajouter" type="button">Ajouter</button>
+        <button class="button-modifier" type="button">Modifier</button>
+      </div>
+      <hr>
+    </div>
+    <!-- Deuxième post identique pour l'exemple -->
+    <div class="post">
+      <div class="titre-logo">
+        <i class="fa-regular fa-message"></i>
+        <h3>TD1 - Validation/Remise à Niveau</h3>
+      </div>
+      <p>Posté par M.Dupont il y a 2j</p>
+      <div class="contenu-message"> Le prochain cours se déroulera en B204 </div>
+      <div class="ajouter-modifier">
+        <button class="button-ajouter" type="button">Ajouter</button>
+        <button class="button-modifier" type="button">Modifier</button>
+      </div>
+      <hr>
+    </div>
+  </section>
+  <!-- ====== Fin section : Posts ====== -->
 </main>
 
 
