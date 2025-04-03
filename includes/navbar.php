c7a4ebdd88005ad8b11305bd3f9b954168afebfd
VarmazE <varmazea@gmail.com>
2025-04-03 02:15:24 +0200

    Reglage navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -3,7 +3,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
-    <link href="../style/navbar.css" rel="stylesheet">
+    <link href="/Soodle/style/navbar.css" rel="stylesheet">
 
     <script src="https://kit.fontawesome.com/4162108d67.js" crossorigin="anonymous"></script>
 </head>
@@ -11,13 +11,13 @@
 <body>
     <div class="navbar">
         <div class="navbar-left">
-            <img class=logo-navbar src="../assets/soodle-s-nobg.png" alt="logo Soodle">
+            <img class=logo-navbar src="/Soodle/assets/soodle-s-nobg.png" alt="logo Soodle">
             <p class="pages-navbar"> <a class="link-to-page" href="/Soodle/cours.php"> Mes cours </a> </p>
             <p class="pages-navbar"> <a class="link-to-page" href="/Soodle/admin.php"> Admin </a> </p>
         </div>
         <div class="navbar-right">
             <i class="fa-regular fa-bell"></i>
-            <img id="profil-picture" class="avatar" src="../assets/avatar.jpg" alt="avatar">
+            <img id="profil-picture" class="avatar" src="/Soodle/assets/avatar.jpg" alt="avatar">
         </div>
     </div>
     <div id="profil-popup">
