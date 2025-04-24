5d5977da3619cb616d220804397417680959fa18
Enesinho90 <enesdu90800@gmail.com>
2025-04-24 12:01:59 +0200

    Ajout formulaire étudiant + navbar changé

--- src/Controller/CreateUserController.php
+++ src/Controller/CreateUserController.php
@@ -2,17 +2,57 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class CreateUserController extends AbstractController
+class CreateUserController extends AbstractController
 {
     #[Route('/admin/createUser', name: 'app_createUser')]
-    public function index(): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('create_user/index.html.twig', [
-            'controller_name' => 'CreateUserController',
+        $user = new Utilisateur();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var string $plainPassword */
+            $plainPassword = $form->get('plainPassword')->getData();
+
+            // encode the plain password
+            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+
+            $roleChoice = $form->get('roleChoice')->getData();
+
+            switch ($roleChoice) {
+                case 'admin':
+                    $user->setRoles(['ROLE_ADMIN']);
+                    break;
+                case 'prof':
+                    $user->setRoles(['ROLE_PROF']);
+                    break;
+                case 'etudiant':
+                    $user->setRoles(['ROLE_ETUDIANT']);
+                    break;
+                case 'admin_prof':
+                    $user->setRoles(['ROLE_PROF','ROLE_ADMIN']);
+                    break;
+            }
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('app_admin');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form,
         ]);
     }
 }
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Utilisateur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('email')
+            ->add('roleChoice', ChoiceType::class, [
+                'mapped' => false, // important car ce champ n’est pas directement relié à l’attribut "roles"
+                'choices'  => [
+                    'Etudiant' => 'etudiant',
+                    'Professeur' => 'prof',
+                    'Administrateur' => 'admin',
+                    'Administrateur / Professeur' => 'admin_prof',
+                ],
+                'label' => 'Rôle',
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Utilisateur::class,
+        ]);
+    }
+}
